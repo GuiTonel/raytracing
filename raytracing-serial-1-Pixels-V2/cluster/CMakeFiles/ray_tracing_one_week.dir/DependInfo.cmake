73a15d633abe354a67d64d6fd87d2859d044a779
Guilherme Tonello <a57064@alunos.ipb.pt>
2023-12-15 17:02:21 +0000

    first commit

---
+++ raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_aa_rect.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_aa_rect.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_box.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_box.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_bvh.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_bvh.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_const_medium.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_const_medium.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_hittable.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_hittable.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_hittable_list.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_hittable_list.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_instance.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_instance.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_moving_sphere.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_moving_sphere.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/hittables/rt_sphere.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/hittables/rt_sphere.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/main.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/main.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/materials/rt_material.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/materials/rt_material.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/materials/rt_material_dielectric.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/materials/rt_material_dielectric.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/materials/rt_material_diffuse.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/materials/rt_material_diffuse.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/materials/rt_material_diffuse_light.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/materials/rt_material_diffuse_light.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/materials/rt_material_isotropic.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/materials/rt_material_isotropic.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/materials/rt_material_metal.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/materials/rt_material_metal.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/rt_aabb.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/rt_aabb.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/rt_camera.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/rt_camera.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/rt_colour.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/rt_colour.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/rt_hit.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/rt_hit.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/rt_perlin.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/rt_perlin.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/rt_skybox_simple.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/rt_skybox_simple.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/scenes/rt_scenes.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/scenes/rt_scenes.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/textures/rt_texture.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/textures/rt_texture.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/textures/rt_texture_checker_pattern.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/textures/rt_texture_checker_pattern.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/textures/rt_texture_image.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/textures/rt_texture_image.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/textures/rt_texture_noise.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/textures/rt_texture_noise.c.o"
+  "/home/ac_group06/raytracing-serial-1-Pixels-V2/textures/rt_texture_solid_colour.c" "/home/ac_group06/raytracing-serial-1-Pixels-V2/cluster/CMakeFiles/ray_tracing_one_week.dir/textures/rt_texture_solid_colour.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../materials"
+  "../hittables"
+  "../textures"
+  "../deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
